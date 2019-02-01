7fa90e0483142358ac8f01f3c0708d36c84ca086
Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2019-02-01 19:56:21 +0100

    next steps

--- src/main/java/XsltJsonReader.java
+++ src/main/java/XsltJsonReader.java
@@ -1,9 +1,12 @@
 import java.io.IOException;
+import java.util.Map;
 
 import javax.json.Json;
 import javax.json.JsonException;
 import javax.json.stream.JsonParser;
+import javax.json.stream.JsonParser.Event;
 
+import org.xml.sax.Attributes;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.DTDHandler;
 import org.xml.sax.EntityResolver;
@@ -13,8 +16,16 @@ import org.xml.sax.SAXException;
 import org.xml.sax.SAXNotRecognizedException;
 import org.xml.sax.SAXNotSupportedException;
 import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.AttributesImpl;
 
 public class XsltJsonReader implements XMLReader {
+  
+  private static final Map<String, Boolean> FEATURES = Map.of(
+    "http://xml.org/sax/features/namespaces", true,
+    "http://xml.org/sax/features/namespace-prefixes", false
+  );
+
+  private static final String NS = "http://www.w3.org/2005/xpath-functions";
 
   private EntityResolver entityResolver;
   private DTDHandler dtdHandler;
@@ -23,22 +34,26 @@ public class XsltJsonReader implements XMLReader {
 
   @Override
   public boolean getFeature(String name) throws SAXNotRecognizedException, SAXNotSupportedException {
-    throw new SAXNotRecognizedException("Feature: " + name);
+    Boolean value = FEATURES.get(name);
+    if (value == null) throw new SAXNotRecognizedException(name);
+    return value.booleanValue();
   }
 
   @Override
-  public void setFeature(String name, boolean value) throws SAXNotRecognizedException, SAXNotSupportedException {
-    throw new SAXNotRecognizedException("Feature: " + name);
+  public void setFeature(String name, boolean v) throws SAXNotRecognizedException, SAXNotSupportedException {
+    Boolean value = FEATURES.get(name);
+    if (value == null) throw new SAXNotRecognizedException(name);
+    if (v != value.booleanValue()) throw new SAXNotSupportedException(name + " = " + v);
   }
 
   @Override
   public Object getProperty(String name) throws SAXNotRecognizedException, SAXNotSupportedException {
-    throw new SAXNotRecognizedException("Property: " + name);
+    throw new SAXNotRecognizedException(name);
   }
 
   @Override
   public void setProperty(String name, Object value) throws SAXNotRecognizedException, SAXNotSupportedException {
-    throw new SAXNotRecognizedException("Property: " + name);
+    throw new SAXNotRecognizedException(name);
   }
 
   @Override
@@ -84,7 +99,65 @@ public class XsltJsonReader implements XMLReader {
   @Override
   public void parse(InputSource input) throws IOException, SAXException {
     try {
-      try (JsonParser parser = Json.createParser(input.getCharacterStream())) {
+      try (JsonParser parser = Json.createParser(input.getByteStream())) {
+        handler.startDocument();
+        handler.startPrefixMapping("", NS);
+        AttributesImpl keyAttrs = new AttributesImpl();
+        keyAttrs.addAttribute("", "key", "key", "CDATA", null);
+        AttributesImpl noAttrs = new AttributesImpl();
+        Attributes attrs = noAttrs;
+        while (parser.hasNext()) {
+          Event event = parser.next();
+          switch (event) {
+          case START_OBJECT:
+            handler.startElement(NS, "map", "map", attrs);
+            attrs = noAttrs;
+            break;
+          case END_OBJECT:
+            handler.endElement(NS, "map", "map");
+            break;
+          case START_ARRAY:
+            handler.startElement(NS, "array", "array", attrs);
+            attrs = noAttrs;
+            break;
+          case END_ARRAY:
+            handler.endElement(NS, "array", "array");
+            break;
+          case KEY_NAME:
+            keyAttrs.setValue(0, parser.getString());
+            attrs = keyAttrs;
+            break;
+          case VALUE_STRING:
+            handler.startElement(NS, "string", "string", attrs);
+            attrs = noAttrs;
+            char[] str = parser.getString().toCharArray();
+            handler.characters(str, 0, str.length);
+            handler.endElement(NS, "string", "string");
+            break;
+          case VALUE_NUMBER:
+            handler.startElement(NS, "number", "number", attrs);
+            attrs = noAttrs;
+            char[] num = parser.getString().toCharArray();
+            handler.characters(num, 0, num.length);
+            handler.endElement(NS, "number", "number");
+            break;
+          case VALUE_TRUE:
+          case VALUE_FALSE:
+            handler.startElement(NS, "boolean", "boolean", attrs);
+            attrs = noAttrs;
+            String bool = event == Event.VALUE_TRUE ? "true" : "false";
+            handler.characters(bool.toCharArray(), 0, bool.length());
+            handler.endElement(NS, "boolean", "boolean");
+            break;
+          case VALUE_NULL:
+            handler.startElement(NS, "null", "null", attrs);
+            attrs = noAttrs;
+            handler.endElement(NS, "null", "null");
+            break;
+          }
+        }
+        handler.endPrefixMapping("");
+        handler.endDocument();
       }
     }
     catch (JsonException e) {
--- src/test/java/XsltJsonReaderTest.java
+++ src/test/java/XsltJsonReaderTest.java
@@ -1,18 +1,32 @@
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.io.IOException;
-import java.io.StringReader;
+import java.io.InputStream;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.junit.jupiter.api.Test;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
 
 class XsltJsonReaderTest {
 
   @Test
-  void testParse() throws IOException, SAXException {
-    InputSource source = new InputSource(new StringReader("{}"));
-    new XsltJsonReader().parse(source);
+  void testParse() throws IOException, SAXException, TransformerException {
+    try (InputStream in = getClass().getResourceAsStream("test.json")) {
+      InputSource input = new InputSource(in);
+      XMLReader reader = new XsltJsonReader();
+      SAXSource source = new SAXSource(reader, input);
+      StreamResult result = new StreamResult(System.out);
+      Transformer transformer = TransformerFactory.newInstance().newTransformer();
+      transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+      transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+      transformer.transform(source, result);
+    }
   }
-
+  
 }
