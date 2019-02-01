f6551a93f425b04ab2367f3621aefcbd58f8db39
Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2019-02-01 17:26:55 +0100

    XsltJsonReaderTest.java

--- src/test/java/XsltJsonReaderTest.java
+++ src/test/java/XsltJsonReaderTest.java
@@ -1,11 +1,18 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+import java.io.StringReader;
+
 import org.junit.jupiter.api.Test;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 class XsltJsonReaderTest {
 
   @Test
-  void testParse() {
+  void testParse() throws IOException, SAXException {
+    InputSource source = new InputSource(new StringReader("{}"));
+    new XsltJsonReader().parse(source);
   }
 
 }
